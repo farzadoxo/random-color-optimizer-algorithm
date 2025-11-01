9da541345be6ed78b2b00eb74b3c5b718bd725b5
Farzad Ebrahimi <farzadebrahimi.oxo@gmail.com>
2025-11-01 20:07:38 +0330

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,3 +1,5 @@
+import random
+
 class Color:
 	def __init__(self,r:int,g:int,b:int):
 		self.r = r
@@ -6,21 +8,23 @@ class Color:
 
 
 class Check:
-	
 	def red_checker(bg:Color,fg:Color):
-		if bg.r - fg.r <   10:
-			return 0 
-		return 1
+		if bg.r - fg.r >  20:
+			return 1
+		else: 
+			return 0
 	
 	def blue_checker(bg:Color,fg:Color):
-		if bg.b - fg.b < 10:
+		if bg.b - fg.b > 20:
+			return 1
+		else:
 			return 0
-		return 1
 	
 	def green_checker(bg:Color,fg:Color):
-		if bg.g - fg.g < 10:
+		if bg.g - fg.g > 20:
+			return 1
+		else:
 			return 0
-		return 1
 
 
 class Pallate:
@@ -29,19 +33,19 @@ class Pallate:
 		self.fg = fg
 
 	def check(self):
-		if Check.red_checker(self.bg,self.fg ) == 0 and Check.blue_checker(self.bg,self.fg) == 0 and Check.green_checker(self.bg,self.fg) == 0:
-			return False
+		if Check.red_checker(self.bg,self.fg ) == 1 and Check.blue_checker(self.bg,self.fg) == 1 and Check.green_checker(self.bg,self.fg) == 1:
+			return True
 		# elif Check.blue_checker(self.bg,self.fg) == 0:
 		# 	return False
 		# elif Check.green_checker(self.bg,self.fg) == 0:
 		# 	return False
 		
 		else:
-			return True
+			return False
 		
 			
-color_01 = Color(r=100,g=100,b=100)
-color_02 = Color(r=6,g=3,b=40)
+color_01 = Color(r=random.randint(1,50),g=random.randint(1,50),b=random.randint(1,50))
+color_02 = Color(r=random.randint(1,50),g=random.randint(1,50),b=random.randint(1,50))
 
 p = Pallate(bg=color_01,fg=color_02)
 
